d54e7c4259cabf6c0a8f3eeee67be4cd145c20bf
Fimmeh <temezzz@hotmail.com>
2025-01-28 09:05:40 +0200

    Added a test scene and first script
    Scene - Script - Namespace

---
+++ Assets/FPS/Scripts/My Scripts/ShootingTarget.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace AG3953
+{
+    public class ShootingTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform[] targetEndPoints;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
